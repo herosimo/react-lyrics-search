0a5efab0502d8f536bc6b2e8e5e3e87c6540489a
Herosimo Sribiko <herosimo06@gmail.com>
2020-05-01 11:12:55 +0700

    Updates

---
+++ precache-manifest.31fe4d3342ab75a3d9543b5a8b7fee69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39afdd4c6b936bb23d7fc95f4c2ad3d0",
+    "url": "/react-lyrics-search/index.html"
+  },
+  {
+    "revision": "319c35248db26d6125e0",
+    "url": "/react-lyrics-search/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b9cc9ddd31386d420d39",
+    "url": "/react-lyrics-search/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "319c35248db26d6125e0",
+    "url": "/react-lyrics-search/static/js/2.de4f481f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lyrics-search/static/js/2.de4f481f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cc9ddd31386d420d39",
+    "url": "/react-lyrics-search/static/js/main.6a605bb5.chunk.js"
+  },
+  {
+    "revision": "3ce07ec952b1caa81f5d",
+    "url": "/react-lyrics-search/static/js/runtime-main.79e1c265.js"
+  }
+]);
